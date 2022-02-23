546a319f00ec6ccc1fee188f8dd9e1f8024de290
Kyle Sunden <git@ksunden.space>
2022-02-23 16:39:35 -0600

    Add a bluesky Readable compliant object which works in RE

--- bluesky_hwproxy/comms/__init__.py
+++ bluesky_hwproxy/comms/__init__.py
@@ -1,5 +1,24 @@
 import functools
 
-from bluesky_queueserver.manager.comms import zmq_single_request
+from bluesky_queueserver.manager.comms import zmq_single_request, ZMQCommSendThreads
 
 zmq_single_request = functools.partial(zmq_single_request, zmq_server_address="tcp://localhost:60620")
+
+class ZMQCommSendThreads(ZMQCommSendThreads):
+    def __init__(
+        self,
+        *,
+        zmq_server_address=None,
+        timeout_recv=2000,
+        timeout_send=500,
+        raise_exceptions=True,
+        server_public_key=None,
+    ):
+        super().__init__(
+            zmq_server_address=zmq_server_address or "tcp://localhost:60620",
+            timeout_recv=timeout_recv,
+            timeout_send=timeout_send,
+            raise_exceptions=raise_exceptions,
+            server_public_key=server_public_key,
+        )
+
